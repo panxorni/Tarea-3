fdf51db6a1c04f5cf0b634e4491a7309bb6979f6
Gaeo24 <hr7xscan@gmail.com>
2026-06-05 22:01:31 -0400

    feat: agrega clase ConstantesGUI para facilitar cambios

---
+++ tarea-3/src/main/java/expendedor/gui/ConstantesGUI.java
@@ -0,0 +1,22 @@
+package expendedor.gui;
+
+public final class ConstantesGUI {
+
+    private ConstantesGUI(){}
+
+    //Ventana
+    public static final int VENTANA_ANCHO = 1000;
+    public static final int VENTANA_ALTO = 700;
+
+    //Expendedor
+    public static final int EXPENDEDOR_X = 0;
+    public static final int EXPENDEDOR_Y = 0;
+    public static final int EXPENDEDOR_ANCHO = 0;
+    public static final int EXPENDEDOR_ALTO = 0;
+
+    //Comprador
+    public static final int COMPRADOR_X = 0;
+    public static final int COMPRADOR_Y = 0;
+    public static final int COMPRADOR_ANCHO = 0;
+    public static final int COMPRADOR_ALTO = 0;
+}
