6e8615d0bc2404138bcd0661219e770f5cd18ec6
Gaeo24 <hr7xscan@gmail.com>
2026-06-06 19:32:07 -0400

    feat: implementa MainPrueba

--- tarea-3/src/main/java/expendedor/gui/PanelPrincipal.java
+++ tarea-3/src/main/java/expendedor/gui/PanelPrincipal.java
@@ -6,12 +6,11 @@ import java.awt.*;
 //se debe implementar metodos paintComponent() para PanelComprador y PanelExpendedor
 
 public class PanelPrincipal extends JPanel {
-    /*
     private PanelComprador comprador;
     private PanelExpendedor expendedor;
     public PanelPrincipal () {
-        expendedor = new PanelExpendedor (..);
-        comprador = new PanelComprador(..);
+        //expendedor = new PanelExpendedor (..);
+        comprador = new PanelComprador();
         this.setBackground(Color.white);
     }
     public void paintComponent (Graphics g) {  //todo se dibuja a partir de este método
@@ -19,8 +18,8 @@ public class PanelPrincipal extends JPanel {
         //el de la super clase solo pinta el fondo (background)
 
         comprador.paintComponent(g); //llama al metodo paintComponent definido en el PanelComprador
-        expendedor.paintComponent(g); //llama al metodo paintComponent definido en el PanelExpendedor
+        //expendedor.paintComponent(g); //llama al metodo paintComponent definido en el PanelExpendedor
     }
-     */
+
 }
 
