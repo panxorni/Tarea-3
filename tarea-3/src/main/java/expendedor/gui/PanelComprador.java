3f8e5e902cceadb316afbdb0501abb0d9c4c47fe
Gaeo24 <hr7xscan@gmail.com>
2026-06-05 22:35:03 -0400

    feat: implementa estructura de PanelComprador

--- tarea-3/src/main/java/expendedor/gui/PanelComprador.java
+++ tarea-3/src/main/java/expendedor/gui/PanelComprador.java
@@ -1,6 +1,28 @@
 package expendedor.gui;
 
 
+import java.awt.*;
+
 public class PanelComprador{
+
+    private int x;
+    private int y;
+    private int anchoPanel;
+    private int altoPanel;
+
+    public PanelComprador(){
+
+        x=ConstantesGUI.COMPRADOR_X;
+        y=ConstantesGUI.COMPRADOR_Y;
+        anchoPanel=ConstantesGUI.COMPRADOR_ANCHO;
+        altoPanel=ConstantesGUI.COMPRADOR_ALTO;
+    }
+
+    public void paintComponent(Graphics g){
+
+        g.setColor(Color.LIGHT_GRAY);
+        g.fillRect(x, y, anchoPanel, altoPanel);
+
+    }
 
 }
