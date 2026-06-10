717dd13be432f46ea982a13ad1058220c68f4fe7
Matías Vicente Pareja Escalona <matyparejaescalona@gmail.com>
2026-06-10 15:35:53 -0400

    Creo clase PanelPrincipal y Producto modificado para la interfaz

--- tarea-3/src/main/java/expendedor/gui/PanelPrincipal.java
+++ tarea-3/src/main/java/expendedor/gui/PanelPrincipal.java
@@ -2,18 +2,28 @@ package expendedor.gui;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 
 //se debe implementar metodos paintComponent() para PanelComprador y PanelExpendedor
 
 public class PanelPrincipal extends JPanel {
-    /*
+
     private PanelComprador comprador;
     private PanelExpendedor expendedor;
     public PanelPrincipal () {
-        expendedor = new PanelExpendedor (..);
-        comprador = new PanelComprador(..);
+        expendedor = new PanelExpendedor ();
+        comprador = new PanelComprador();
         this.setBackground(Color.white);
+        this.addMouseListener(new MouseAdapter(){
+            @Override
+            public void mouseClicked(MouseEvent e){
+                comprador.procesoClick(e.getX(), e.getY());
+                expendedor.procesoClick(e.getX(), e.getY());
+                repaint();
+            }
+        });
     }
+    @Override
     public void paintComponent (Graphics g) {  //todo se dibuja a partir de este método
         super.paintComponent(g); //llama al método paint al que hace override en la super clase
         //el de la super clase solo pinta el fondo (background)
@@ -21,6 +31,5 @@ public class PanelPrincipal extends JPanel {
         comprador.paintComponent(g); //llama al metodo paintComponent definido en el PanelComprador
         expendedor.paintComponent(g); //llama al metodo paintComponent definido en el PanelExpendedor
     }
-     */
 }
 
--- tarea-3/src/main/java/expendedor/logica/Producto.java
+++ tarea-3/src/main/java/expendedor/logica/Producto.java
@@ -1,11 +1,46 @@
 package expendedor.logica;
+
+import java.awt.*;
+
 /** Este es el encargado de representar un producto vendible por la maquina expendedora.
  * Esta sera la clase principal de las que derivan cada producto, ya que de esta heredaran tanto Bebida como Dulce
  * y de estas, sus subclases.*/
+
 abstract public class Producto {
     protected TipoProducto tipo;
+    protected int x;
+    protected int y;
+    protected int ancho;
+    protected int alto;
+    protected static int contadorSerie=0;
+    protected int serie;
+
     public Producto (TipoProducto tipo){
+        this.x=0;
+        this.y=0;
+        this.alto=40;
+        this.ancho=80;
+        contadorSerie++;
+        this.serie=contadorSerie;
         this.tipo=tipo;
+    }
+    public void setXY (int x, int y){
+        this.x=x;
+        this.y=y;
+    }
+    public int getSerie(){
+        return serie;
+    }
+    public TipoProducto getTipo(){
+        return tipo;
+    }
+    public void paintComponent (Graphics g){
+        g.setColor(Color.BLACK);
+        g.fillRect(x,y,ancho,alto);
+        g.setColor(Color.WHITE);
+        g.drawRect(x,y,ancho,alto);
+        g.drawString(tipo.getNombre(),x+5,y+15);
+        g.drawString("S:" + serie, x+5, y+30);
     }
     abstract public String consumir();
 }
