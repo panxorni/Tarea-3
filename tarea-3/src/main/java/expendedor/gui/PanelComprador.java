2ecb610e65ca3ad8ded45052d3b143f604a34601
Gaeo24 <hr7xscan@gmail.com>
2026-06-12 16:39:52 -0400

    fix: arregla cambios que se habian perdido

--- tarea-3/src/main/java/expendedor/gui/PanelComprador.java
+++ tarea-3/src/main/java/expendedor/gui/PanelComprador.java
@@ -105,27 +105,33 @@ public class PanelComprador{
             b.draw(g);
         }
         g.setColor(Color.WHITE);
-        g.drawString("Producto seleccionado: " + (productoSeleccionado == null ? "Ninguno" : productoSeleccionado.name()), x + 20, y + altoPanel - 20);
+        g.drawString("Producto seleccionado: ", x + 20,y + 280);
+        g.drawString( (productoSeleccionado == null ? "Ninguno" : productoSeleccionado.name()), x + 20, y + 290);
 
         for(MonedaGUI moneda : monedero){
             moneda.paintComponent(g);
         }
         g.setColor(Color.WHITE);
-        g.drawString("Monedero", x + 20, y + 280);
-        g.drawString("Moneda: " + (monedaSeleccionada == null ? "Ninguna" : "$" + monedaSeleccionada.getMoneda().getValor()), x + 20, y + altoPanel - 40);
-        g.setColor(Color.WHITE);
+        g.drawString("Monedero", x + 20, y + altoPanel-120);
 
-        g.drawString("Productos comprados:", x + 200, y + 40);
+        g.setColor(Color.lightGray);
+        g.fillRect(x+200, y+20, 160, 290);
+        g.setColor(Color.BLACK);
+        g.drawString("Productos comprados:", x + 220, y + 40);
         int productoY = y + 60;
-        int productoX = x + 200;
+        int productoX = x + 215;
         for(Producto p : productosComprados){
             ProductoGUI productoGUI = new ProductoGUI(p);
 
             productoGUI.setXY(productoX, productoY);
 
             productoGUI.paintComponent(g);
-            //g.drawString(p.getClass().getSimpleName(), productoX + 20, productoY);
             productoY += 60;
+
+            if(productoY > y + 280){
+                productoX += 75;
+                productoY = y+60;
+            }
         }
 
         g.setColor(Color.RED);
@@ -158,7 +164,7 @@ public class PanelComprador{
 
     private void generarMonedas(){
 
-        monedero.add(new MonedaGUI(new Moneda1000()));
+        monedero.add(new MonedaGUI(new Moneda1500()));
 
         monedero.add(new MonedaGUI(new Moneda1000()));
 
@@ -167,16 +173,18 @@ public class PanelComprador{
     private void posicionarMonedero(){
 
         int monedaX = x + 20;
-        int monedaY = y + 300;
+        int monedaY = y + altoPanel-110;
 
         for(MonedaGUI moneda : monedero){
 
-            moneda.setXY(
-                    monedaX,
-                    monedaY
-            );
+            moneda.setXY(monedaX, monedaY);
 
             monedaX += 60;
+
+            if(monedaX>=x+anchoPanel-10){
+                monedaX=x+20;
+                monedaY+=50;
+            }
         }
     }
 
